1189dc34b8eb38d0e5613a681bfe283e97b52012
HKing <amc7082@rit.edu>
2020-09-26 15:50:31 -0400

    corrected selectors for login

--- app/web/login.js
+++ app/web/login.js
@@ -1,11 +1,18 @@
 // JavaScript source code
 $(document).ready(function() {
-    $(".login").click(function () {
-        var user = $("#user")
-        var pass = $("#pass")
+    $(".btn btn-primary btn-block").click(function () {
+        var user = $("#username")
+        var pass = $("#password")
         $.post("action/login", {
             username: user,
             password: pass
+        }, function (msg) {
+                if (msg.error) {
+                    window.location("error.html")
+                    alert(msg.msg)
+                } else {
+                    window.location("dashboard.html")
+                }
         });
     });
 });
