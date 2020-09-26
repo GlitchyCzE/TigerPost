d68a87b55a0c77d6596a3164e9fddf0fe8cb4b23
HKing <amc7082@rit.edu>
2020-09-26 17:50:13 -0400

    fixed admin.js

--- app/web/admin.js
+++ app/web/admin.js
@@ -1,7 +1,8 @@
 // JavaScript source code
 $(document).ready(function () {
     $.post("action/getPackages", {},
-        function (packages) {
+        function (data) {
+            let packages = data.data
             for (var i = 0; i < packages.length; i++) {
                 var markup = "<tr><td>" + packages[i].pid + "</td><td>" + packages[i].tid + "</td><td>" + packages[i].to + "</td><td>" + packages[i].address + "</td><td>" + packages[i].time_scanned_in + "</td><td>" + packages[i].scanned_in_by + "</td><td>" + packages[i].stored_in + "</td><td>" + packages[i].checkout_by + "</td><td>" + packages[i].checkout_time + "</td></tr>";
                 $("#ready tbody").append(markup);
