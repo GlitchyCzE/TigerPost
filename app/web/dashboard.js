8f4aa805275c91d782c2d4994f9a4bc6c73e5d81
GlitchyCzE <cze@clashmc.co>
2020-09-26 17:40:29 -0400

    Attempt on fixing package data

--- app/web/dashboard.js
+++ app/web/dashboard.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
         function (data) {
             let packages = data.data;
             for (var i = 0; i < packages.length; i++) {
-                var markup = "<td>" + packages[i].tid + "</td><td>" + packages[i].to + "</td><td>" + packages[i].address + "</td><td>" + packages[i].time_scanned_in + "</td><td>";
+                var markup = "<tr></tr><td>" + packages[i].tid + "</td><td>" + packages[i].to + "</td><td>" + packages[i].address + "</td><td>" + packages[i].time_scanned_in + "</td><td>";
                 if (packages[i].checkout_time == null) {
-                    $("#ready tbody").append(markup + "Ready for Pickup</td>");
+                    $("#ready tbody").append(markup + "Ready for Pickup</td></tr>");
                 } else {
-                    $("#archive tbody").append(markup + "Picked up</td>");
+                    $("#archive tbody").append(markup + "Picked up</td></tr>");
                 }
             }
             $("#ready #dummyRow").remove();
