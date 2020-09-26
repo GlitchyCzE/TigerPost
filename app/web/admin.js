6f9d68098b61179114cc66c24c192877ed13323e
HKing <amc7082@rit.edu>
2020-09-26 17:48:46 -0400

    fixed logout button

--- app/web/admin.js
+++ app/web/admin.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     $.post("action/getPackages", {},
         function (packages) {
             for (var i = 0; i < packages.length; i++) {
-                var markup = "<td>" + packages[i].pid + "</td><td>" + packages[i].tid + "</td><td>" + packages[i].to + "</td><td>" + packages[i].address + "</td><td>" + packages[i].time_scanned_in + "</td><td>" + packages[i].scanned_in_by + "</td><td>" + packages[i].stored_in + "</td><td>" + packages[i].checkout_by + "</td><td>" + packages[i].checkout_time + "</td>";
+                var markup = "<tr><td>" + packages[i].pid + "</td><td>" + packages[i].tid + "</td><td>" + packages[i].to + "</td><td>" + packages[i].address + "</td><td>" + packages[i].time_scanned_in + "</td><td>" + packages[i].scanned_in_by + "</td><td>" + packages[i].stored_in + "</td><td>" + packages[i].checkout_by + "</td><td>" + packages[i].checkout_time + "</td></tr>";
                 $("#ready tbody").append(markup);
             }
             $("#ready #dummyRow").remove();
         });
-    $(".btn btn-primary btn-block btn-sm").click(function (event) {
+    $("#logout").click(function (event) {
         event.preventDefault();
         $.post("action/logout");
         window.location = "index.html";
