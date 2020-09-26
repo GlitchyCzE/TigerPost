d0b8a38dc33e6be94d738657f88faad223a360f0
GlitchyCzE <cze@clashmc.co>
2020-09-26 14:17:52 -0400

    Created config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    localPort: 3000,
+
+    database: {
+        host: "localhost",
+        username: "tigerpost",
+        password: "changeme"
+    }
+}
+
+module.exports = CONFIG;
