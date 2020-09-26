ad34ee2ac1e8e01f9ebb9a1fa4a75b7e6ad47fc7
Zach <cze@clashmc.co>
2020-09-26 16:16:21 -0400

    Update config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    localPort: 3000,
+
+    database: {
+        host: "localhost",
+        database: "tigerpost",
+        username: "tigerpost",
+        password: "changeme"
+    }
+}
+
+module.exports = CONFIG;
