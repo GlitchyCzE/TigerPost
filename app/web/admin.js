1eff3069259dc59b2b9075eb72725d4ec25898aa
HKing <amc7082@rit.edu>
2020-09-26 16:30:01 -0400

    admin page logic

---
+++ app/web/admin.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+$(document).ready(function () {
+    $.post("action/getPackages", {},
+        function (packages) {
+            for (var i = 0; i < packages.length; i++) {
+                var markup = "<td>" + packages[i].pid + "</td><td>" + packages[i].tid + "</td><td>" + packages[i].to + "</td><td>" + packages[i].address + "</td><td>" + packages[i].time_scanned_in + "</td><td>" + packages[i].scanned_in_by + "</td><td>" + packages[i].stored_in + "</td><td>" + packages[i].checkout_by + "</td><td>" + packages[i].checkout_time + "</td>"
+                $("#ready tbody").append(markup);
+            }
+            $("#ready #dummyRow").remove();
+        });
+    $(".btn btn-primary btn-block btn-sm").click(function () {
+        $.post("action/logout")
+    });
+});
