1d435a3abcf4c787da622b394ffffd623031d6e3
GlitchyCzE <cze@clashmc.co>
2020-09-26 15:20:57 -0400

    Fixed database setup .sql file

--- app/server/database.sql
+++ app/server/database.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `users` (
-	`uid` INT NOT NULL,
+	`uid` INT NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(32) NOT NULL,
 	`hash` VARCHAR(256) NOT NULL,
 	`salt` VARCHAR(32) NOT NULL,
 	`email` VARCHAR(100) NOT NULL,
 	`fullname` VARCHAR(100) NOT NULL,
 	`is_admin` BOOLEAN NOT NULL DEFAULT '0',
-	UNIQUE KEY `PRIMARY` (`uid`) USING BTREE,
 	PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB;
 
@@ -14,7 +13,6 @@ CREATE TABLE `locations` (
 	`lid` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(32) NOT NULL,
 	`description` TEXT,
-	UNIQUE KEY `PRIMARY` (`lid`) USING BTREE,
 	PRIMARY KEY (`lid`)
 ) ENGINE=InnoDB;
 
@@ -28,6 +26,5 @@ CREATE TABLE `packages` (
 	`stored_in` INT,
 	`checkout_by` INT,
 	`checkout_time` INT,
-	UNIQUE KEY `PRIMARY` (`pid`) USING BTREE,
 	PRIMARY KEY (`pid`)
 ) ENGINE=InnoDB;
