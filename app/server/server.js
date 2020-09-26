91ddc57ffc0f00f355fa6e307a9ff2633bfe8194
GlitchyCzE <cze@clashmc.co>
2020-09-26 14:00:17 -0400

    Layed out basics for express webserver

--- app/server/server.js
+++ app/server/server.js
@@ -1,1 +1,47 @@
 // Root of the server
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('../web'));
+
+app.post('/action/login', (req, res) => {
+
+});
+
+app.post('/action/getPackages', (req, res) => {
+
+});
+
+app.post('/action/createPackage', (req, res) => {
+
+});
+
+app.post('/action/storePackage', (req, res) => {
+
+});
+
+app.post('/action/checkoutPackage', (req, res) => {
+
+});
+
+app.post('/action/getLocations', (req, res) => {
+
+});
+
+app.post('/action/createLocation', (req, res) => {
+
+});
+
+app.post('/action/updateLocation', (req, res) => {
+
+});
+
+app.post('/action/deleteLocation', (req, res) => {
+
+});
+
+app.listen(port, () => {
+    console.log(`TigerPost listening locally on http://localhost:${port}`);
+    console.log(`One must start up the proxy webserver using the config in /webserver`);
+})
